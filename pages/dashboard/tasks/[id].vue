0b78aa033545bf1b81931e04ff5b0c1349a41927
Kowenjko <ortoswt@gmail.com>
2024-09-18 15:15:16 +0300

    add button close in id page

--- pages/dashboard/tasks/[id].vue
+++ pages/dashboard/tasks/[id].vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-import { Trash2, UserCog, CircleUserRound } from 'lucide-vue-next'
+import { Trash2, UserCog, CircleUserRound, X } from 'lucide-vue-next'
 import { useDashboardStore } from '@/store/dashboardStore'
 import { useToast } from '@/components/ui/toast/use-toast'
 import type { ITask } from '@/types'
 
 const route = useRoute()
+const router = useRouter()
 
 const { toast } = useToast()
 const dashboardStore = useDashboardStore()
@@ -21,6 +22,8 @@ const deleteTask = () => {
 	navigateTo('/dashboard')
 }
 
+const close = () => router.push('/dashboard/')
+
 useHead({
 	title: task.value.title,
 	meta: [{ name: 'description', content: task.value.description }],
@@ -28,15 +31,15 @@ useHead({
 </script>
 
 <template>
-	<div class="max-w-xl bg-gray-200 m-32 mx-auto py-4 rounded-lg px-4">
+	<div class="max-w-xl bg-gray-200 m-32 mx-auto py-4 rounded-lg px-4 relative">
+		<Button @click="close" class="absolute right-4" size="icon" variant="outline"
+			><X :size="16"
+		/></Button>
 		<form v-if="task" class="flex flex-col flex-grow items-start justify-between gap-2">
 			<div class="flex justify-between w-full">
 				<h3 class="pb-4">
 					Edit task <span class="font-bold">{{ task.title }}</span>
 				</h3>
-				<Button @click="deleteTask" type="button" variant="outline" size="icon">
-					<Trash2 color="red" :size="12" />
-				</Button>
 			</div>
 			<div class="grid w-full items-center gap-1.5">
 				<Label class="text-gray-600" for="title">Title</Label>
@@ -70,18 +73,23 @@ useHead({
 					/>
 				</div>
 			</div>
-			<div>
-				<Label class="text-gray-600 block pb-2">Priority</Label>
-				<RadioGroup :default-value="task.priority" v-model="task.priority" class="flex">
-					<div
-						class="flex items-center space-x-2"
-						v-for="item in dashboardStore.priority"
-						:key="item"
-					>
-						<RadioGroupItem :id="item" :value="item" />
-						<Label :for="item">{{ item }}</Label>
-					</div>
-				</RadioGroup>
+			<div class="flex justify-between w-full items-end">
+				<div>
+					<Label class="text-gray-600 block pb-2">Priority</Label>
+					<RadioGroup :default-value="task.priority" v-model="task.priority" class="flex">
+						<div
+							class="flex items-center space-x-2"
+							v-for="item in dashboardStore.priority"
+							:key="item"
+						>
+							<RadioGroupItem :id="item" :value="item" />
+							<Label :for="item">{{ item }}</Label>
+						</div>
+					</RadioGroup>
+				</div>
+				<Button @click="deleteTask" type="button" variant="outline" size="icon">
+					<Trash2 color="red" :size="14" />
+				</Button>
 			</div>
 		</form>
 		<div v-else>
