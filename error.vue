af2f91f88598deb9844f8d3abe2a326e1f0647a2
Kowenjko <ortoswt@gmail.com>
2024-09-18 13:38:34 +0300

    add error page

---
+++ error.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const error = useError()
+
+const handleError = () => {
+	clearError({
+		redirect: '/',
+	})
+}
+</script>
+
+<template>
+	<NuxtLayout>
+		<div class="h-full w-full flex flex-col gap-4 items-center justify-center">
+			<h1 class="text-3xl font-bold">Oops!!!</h1>
+			<p>
+				<strong>{{ error?.message }}</strong>
+			</p>
+			<Button @click="handleError">Go to home</Button>
+		</div>
+	</NuxtLayout>
+</template>
